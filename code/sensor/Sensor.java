7059f13f87ba4ce2c267afd0fde2bb175bfe6a5b
Yi Chen <chenyi1@ecs.vuw.ac.nz>
2024-05-17 22:54:07 +1200

    Update Sensor.java

--- code/sensor/Sensor.java
+++ code/sensor/Sensor.java
@@ -1,12 +1,11 @@
 /*
- * Code made by: Harry Booth-Beach
+ * Code made by: Harry Booth-Beach, with modifications by: Yi Chen
  * Date created: 13/05/2024
- * Date modified: 13/05/2024
+ * Date modified: 17/05/2024
  */
 package code.sensor;
 
 public interface Sensor {
     SensorData sendSensorData();
-    void monitor();
-    void reportError(String err);
+    
 }
