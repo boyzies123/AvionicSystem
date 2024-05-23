a0654ce596da28774cb7e5ff35a2c6482c2011fb
Yi Chen <chenyi1@ecs.vuw.ac.nz>
2024-05-24 11:20:17 +1200

    Replace Sensor.java

--- code/sensor/Sensor.java
+++ code/sensor/Sensor.java
@@ -1,11 +1,21 @@
 /*
- * Code made by: Harry Booth-Beach, with modifications by: Yi Chen
+ * Code made by: Yi Chen
  * Date created: 13/05/2024
- * Date modified: 17/05/2024
+ * Date modified: 13/05/2024
  */
-package code.sensor;
+package sensor;
 
-public interface Sensor {
-    SensorData sendSensorData();
-    
+public class Sensor {
+	private long updateFreq;
+	public Sensor(long updateFreq) {
+		this.updateFreq = updateFreq;
+	}
+	/**
+     * Getter update frequency of sensor
+     * @return
+     */
+    public long getUpdateFrequency() {
+        return this.updateFreq;
+    }
 }
+
