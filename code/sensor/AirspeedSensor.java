8417e51fe30103126b8e2b42dce8e7e7001cb560
Yi Chen <chenyi1@ecs.vuw.ac.nz>
2024-05-24 11:17:15 +1200

    Replace AirspeedSensor.java

--- code/sensor/AirspeedSensor.java
+++ code/sensor/AirspeedSensor.java
@@ -1,56 +1,35 @@
+package sensor;
 /*
  * Code made by: Harry Booth-Beach, with modifications by Yi Chen
- * Date created: 17/05/2024
- * Date modified: 19/05/2024
+ * Date created: 13/05/2024
+ * Date modified: (if modified)
  */
-package code.sensor;
 
-public class AirspeedSensor {
-    private int updateFreq;
-    private double currAirspeed;
+public class AirspeedSensor extends Sensor{
+    private double currentAirspeed;
     private static double MIN_SPEED = 50;
     private static double MAX_SPEED = 500;
-    private double airspeedSensorID;
-
-    /**
-     * Sends data to SensorData class
-     * @param sD
-     */
-    public void sendSensorData(SensorData sD) {
-        sD.setAirspeed(currAirspeed);
+    private double airspeedId;
+    public AirspeedSensor() {
+    	super(1000);
     }
 
-   
-
-    /**
-     * Setter method for current airspeed
-     * @param speed
-     */
-    public void setCurrAirspeed(double speed) {
-        this.currAirspeed = speed;
+    public double getCurrentAirspeed(){
+        return this.currentAirspeed;
     }
-
-    /**
-     * Getter method for current airspeed
-     * @return
-     */
-    public double getCurrAirspeed() {
-        return currAirspeed;
-    }
-    /**
-     * Getter method for the max speed 
-     * @return
-     */
     public static double getMaxSpeed(){
         return MAX_SPEED;
     }
-    /**
-     * Getter method for the min speed
-     * @return
-     */
     public static double getMinSpeed(){
         return MIN_SPEED;
     }
-
-
+    public void setCurrentAirspeed(double currentAirspeed){
+        this.currentAirspeed = currentAirspeed;
+    }
+   
+    public SensorData sendSensorData(){
+        SensorData s = new SensorData();
+        s.setAirspeed(this.airspeedId);
+        return s;
+    }
 }
