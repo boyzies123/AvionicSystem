9536d70a3e1051e46be150d2b7fe183e1d72494f
Yi Chen <chenyi1@ecs.vuw.ac.nz>
2024-05-28 18:56:49 +1200

    Fixing up things

--- code/testing/IntegrationTesting.java
+++ code/testing/IntegrationTesting.java
@@ -1,10 +1,10 @@
-package testing;
+package code.testing;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -12,17 +12,21 @@ import code.autopilot.AutoPilotSystem;
 import code.autopilot.ControlSignal;
 import code.autopilot.ControlSurface;
 import code.autopilot.EngineControlSystem;
-import code.CoreSystem;
+import code.core.CoreSystem;
 import code.sensor.AttitudeSensor;
 import code.sensor.Engine;
 import code.sensor.Sensor;
-import code.sensor.SensorData;
+import code.src.Waypoint;
 import code.ui.AutopilotControlPanel;
 
-import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+/*
+ * Tests made by: Yi Chen
+ * Date created: 25/05/2024
+ * Date modified: 27/05/2024
+ */
 /**
  * These are the unit tests for testing fucntionality of the avionic system after 
  * components are integrated.
@@ -42,8 +46,8 @@ public class IntegrationTesting {
         sensors.add(new AttitudeSensor(50, 50, 50));
         this.controlSurface = new ControlSurface(sensors.get(0), sensors.get(1), sensors.get(2));
         this.engineControlSystem = new EngineControlSystem(new Engine(400, 40), new Engine(400, 40));
-        this.autoPilotSystem = new AutoPilotSystem(this.controlSurface, this.engineControlSystem, true);
-        this.backupAutoPilotSystem = new AutoPilotSystem(this.controlSurface, this.engineControlSystem, true);
+        this.autoPilotSystem = new AutoPilotSystem(this.controlSurface, this.engineControlSystem, true, true);
+        this.backupAutoPilotSystem = new AutoPilotSystem(this.controlSurface, this.engineControlSystem, true, true);
         AutopilotControlPanel.addAutoPilotSystem(this.autoPilotSystem, this.backupAutoPilotSystem);
         //We dont want it to run continously so set autopilot status to false
         autoPilotSystem.setStatus(false);
@@ -63,22 +67,24 @@ public class IntegrationTesting {
     void testSendingControlSignal(){
         ControlSignal csToControlSurface = new ControlSignal(9.8, 19.8, 29.8);
         ControlSignal csToEngineControlSystem = new ControlSignal(100, 120, 100, 120);
-        double oldAileronPosition = csToControlSurface.getAileronPosition();
-        double oldRudderPosition = csToControlSurface.getRudderPosition();
-        double oldElevatorPosition = csToControlSurface.getElevatorPosition();
-        double oldThrustOnePosition = csToEngineControlSystem.getThrustOnePosition();
-        double oldThrustTwoPosition = csToEngineControlSystem.getThrustOnePosition();
-        double oldFuelFlowOnePosition = csToEngineControlSystem.getFuelFlowOne();
-        double oldFuelFlowTwoPosition = csToEngineControlSystem.getFuelFlowTwo();
+        double oldAileronPosition = controlSurface.getAileronPosition();
+        double oldRudderPosition = controlSurface.getRudderPosition();
+        double oldElevatorPosition = controlSurface.getElevatorPosition();
+        double oldThrustOnePosition = engineControlSystem.getThrustOne();
+        double oldThrustTwoPosition = engineControlSystem.getThrustTwo();
+        double oldFuelFlowOnePosition = engineControlSystem.getFuelFlowOne();
+        double oldFuelFlowTwoPosition = engineControlSystem.getFuelFlowTwo();
+        System.out.println(oldAileronPosition);
         this.controlSurface.executeControlSignal(csToControlSurface);
         this.engineControlSystem.executeControlSignal(csToEngineControlSystem);
-        double newAileronPosition = csToControlSurface.getAileronPosition();
-        double newRudderPosition = csToControlSurface.getRudderPosition();
-        double newElevatorPosition = csToControlSurface.getElevatorPosition();
-        double newThrustOnePosition = csToEngineControlSystem.getThrustOnePosition();
-        double newThrustTwoPosition = csToEngineControlSystem.getThrustOnePosition();
-        double newFuelFlowOnePosition = csToEngineControlSystem.getFuelFlowOne();
-        double newFuelFlowTwoPosition = csToEngineControlSystem.getFuelFlowTwo();
+        double newAileronPosition = controlSurface.getAileronPosition();
+        System.out.println(newAileronPosition);
+        double newRudderPosition = controlSurface.getRudderPosition();
+        double newElevatorPosition = controlSurface.getElevatorPosition();
+        double newThrustOnePosition = engineControlSystem.getThrustOne();
+        double newThrustTwoPosition = engineControlSystem.getThrustOne();
+        double newFuelFlowOnePosition = engineControlSystem.getFuelFlowOne();
+        double newFuelFlowTwoPosition = engineControlSystem.getFuelFlowTwo();
         assertTrue(oldAileronPosition != newAileronPosition);
         assertTrue(oldRudderPosition != newRudderPosition);
         assertTrue(oldElevatorPosition != newElevatorPosition);
@@ -99,8 +105,10 @@ public class IntegrationTesting {
         ControlSignal csToEngineControlSystem = new ControlSignal(100, 120, 100, 120);
         this.controlSurface.executeControlSignal(csToControlSurface);
         this.engineControlSystem.executeControlSignal(csToEngineControlSystem);
-
+        //assertNotEquals(((AttitudeSensor)sensors.get(1)).getCurrRoll(), ((AttitudeSensor)sensors.get(2)).getCurrRoll());
+        assertEquals(this.engineControlSystem.sendSensorData().getThrust1(), this.engineControlSystem.getThrustOne());
         // Verify execution
+        assertNotEquals(Math.abs(this.controlSurface.sendSensorData().getRoll() - this.controlSurface.getAileronPosition()) / (this.controlSurface.getAileronPosition()) * 100, 0);
         assertTrue(this.autoPilotSystem.verifyExecution(this.controlSurface.sendSensorData(), this.engineControlSystem.sendSensorData()));
     }
     /**
@@ -115,9 +123,9 @@ public class IntegrationTesting {
         this.controlSurface.executeControlSignal(csToControlSurface);
         this.engineControlSystem.executeControlSignal(csToEngineControlSystem);
         //Alter values of sensor so that it isn't same 
-        ((AttitudeSensor)sensors.get(0)).setRoll(50000);
-        ((AttitudeSensor)sensors.get(1)).setRoll(50000);
-        ((AttitudeSensor)sensors.get(1)).setRoll(50000);
+        ((AttitudeSensor)sensors.get(0)).setRoll(500);
+        ((AttitudeSensor)sensors.get(1)).setRoll(5000);
+        ((AttitudeSensor)sensors.get(2)).setRoll(50000);
         // Verify execution
         assertFalse(this.autoPilotSystem.verifyExecution(this.controlSurface.sendSensorData(), this.engineControlSystem.sendSensorData()));
     }
@@ -133,8 +141,8 @@ public class IntegrationTesting {
         coreSystem.getAutoPilotSystem().healthCheck(1.0, 1.0, 1.0, 0.0, 0.0, 0.0);
         assertTrue(coreSystem.getAutoPilotSystem().errorInAutoPilot());
         coreSystem.checkErrorInAutoPilot();
-        assertTrue(coreSystem.getAutoPilotSystem.getStatus());
-        assertFalse(coreSystem.getBackUpAutoPilotSystem.getStatus())
+        assertFalse(coreSystem.getAutoPilotSystem().checkActive());
+        assertTrue(coreSystem.getbackUpAutoPilotSystem().checkActive());
         
     }
     /**
@@ -143,16 +151,22 @@ public class IntegrationTesting {
      */
     @Test
     public void testAlertPilot() throws InterruptedException {
-        autoPilotSystem.setSimulation(true);
-        autoPilotSystem.start();
-        autoPilotSystem.sendControlSignal();
+        List<Waypoint> waypoints = new ArrayList<Waypoint>();
+
+        waypoints.add(new Waypoint(1, 2, 3, 4, 5));
+        waypoints.add(new Waypoint(5, 6, 3, 6, 5));
+        waypoints.add(new Waypoint(5, 6, 3, 6, 5));
+        AutoPilotSystem autoPilotSystem1 = new AutoPilotSystem(this.controlSurface, this.engineControlSystem, true, true);
+        autoPilotSystem1.setWaypoints(waypoints);
+        autoPilotSystem1.start();
+        autoPilotSystem1.sendControlSignal();
         
         // Allow time for retries
-        Thread.sleep(2000); // Wait to ensure retries happen
+        Thread.sleep(5000); // Wait to ensure retries happen
 
         // Verify that alert has been sent 
-        assertEquals(true, autoPilotSystem.getAlertSent());
-        autoPilotSystem.setSimulation(false);
+        assertEquals(true, autoPilotSystem1.getAlertSent());
+        //autoPilotSystem.setSimulation(false);
     }
     
 }
