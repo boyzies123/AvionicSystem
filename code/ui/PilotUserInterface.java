6335627f5fe0f13d45402a9d06dfecad5e976ac8
Yi Chen <chenyi1@ecs.vuw.ac.nz>
2024-05-28 22:33:03 +1200

    Accidently modified old version of code, upload correct one here

--- code/ui/PilotUserInterface.java
+++ code/ui/PilotUserInterface.java
@@ -1,46 +1,48 @@
-package ui;
+package code.ui;
+
 import javax.swing.*;
-
-import sensor.Sensor;
-
 import java.awt.*;
 
 
 /*
  * Code made by: James McKenzie
  * Date created: 13/05/2024
- * Date modified: (if modified)
+ * Date modified: 20/05/2024
  */
 public class PilotUserInterface extends JFrame{
 
-    private AutopilotControlPanel autopilotControlPanel = new AutopilotControlPanel();
-    private FlightPlanManagement flightPlanManagement = new FlightPlanManagement();
-    private SensorDataDisplay sensorDataDisplay = new SensorDataDisplay();
-    private HazardAlertsDisplay hazardAlertsDisplay = new HazardAlertsDisplay();
+    
     // Visual Display Map
     private JPanel mapPanel;
     
     public PilotUserInterface() {
-    	
+        
     }
-    /**.
-     * Creates and initializes all necessary components
-     * 
-     */
+    /**
+    * Creates and intializes all necessary components
+    */
     public void start() {
-    	setTitle("Aircraft Pilot Interface");
+        setTitle("Aircraft Pilot Interface");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
         
         // Initialize components
 
-        initializeVisualDisplayMap();
+        //initializeVisualDisplayMap();
+        //this.mapPanel = new MapDisplay();
+
+        FlightPlanManagement.initialize();
+        AutopilotControlPanel.initialize();
+        MapDisplay.initialize();
+        HazardAlertsDisplay.initialize();
+        SensorDataDisplay.initialize();
+        
         // Add components to the frame
-        add(flightPlanManagement, BorderLayout.NORTH);
-        add(autopilotControlPanel, BorderLayout.WEST);
-        add(sensorDataDisplay, BorderLayout.CENTER);
-        add(hazardAlertsDisplay, BorderLayout.SOUTH);
-        add(mapPanel, BorderLayout.EAST);
+        add(FlightPlanManagement.getPanel(), BorderLayout.NORTH);
+        add(AutopilotControlPanel.getPanel(), BorderLayout.WEST);
+        add(MapDisplay.getPanel(), BorderLayout.CENTER);
+        add(HazardAlertsDisplay.getPanel(), BorderLayout.SOUTH);
+        add(SensorDataDisplay.getPanel(), BorderLayout.EAST);
         
         pack();
         setVisible(true);
@@ -51,11 +53,12 @@ public class PilotUserInterface extends JFrame{
         mapPanel.setBorder(BorderFactory.createTitledBorder("Visual Display Map"));
         // Add map drawing functionality
     }
-    public AutopilotControlPanel getAutopilotControlPanel(){
-        return autopilotControlPanel;
-    }
+
+    /**public MapDisplay getMapDisplay(){
+        return this.mapPanel;
+    }**/
+    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(PilotUserInterface::new);
     }
-    
 }
