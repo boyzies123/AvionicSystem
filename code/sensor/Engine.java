c2c65b9c5173a61f7eadf99bef72021674008d2e
Yi Chen <chenyi1@ecs.vuw.ac.nz>
2024-05-29 14:03:58 +1200

    Updating layout, adhering to checkstyle requirement

--- code/sensor/Engine.java
+++ code/sensor/Engine.java
@@ -1,26 +1,62 @@
+/*
+ * Code made by: Harry Booth-Beach, Yi Chen
+ * Date created: 20/05/2024
+ * Date modified: 27/05/2024
+ */
+
 package code.sensor;
 
-public class Engine {
-    private double currThrust;
-    private double fuelFlow;
-    public Engine(double currentThrust, double fuelFlow){
-        this.currThrust = currentThrust;
-        this.fuelFlow = fuelFlow;
-    }
-    /**
-     * Getter method for fuel flow
-     * @return
-     */
-    public double getFuelFlow(){
-        return this.fuelFlow;
-    }
-    public double getCurrThrust(){
-        return this.currThrust;
-    }
-    public void setFuelFlow(double fuelFlow){
-        this.fuelFlow = fuelFlow;
-    }
-    public void setCurrThrust(double thrust){
-        this.currThrust = thrust;
-    }
+/**.
+ * Class of engine, containing information about engine parameters
+ */
+public class Engine extends Sensor {
+  private double currThrust;
+  private double fuelFlow;
+  /**.
+   * Constructor of engine which takes in the thrust and fuel
+   * flow of engine
+   */
+  
+  public Engine(double currentThrust, double fuelFlow) {
+    super(1000);
+    this.currThrust = currentThrust;
+    this.fuelFlow = fuelFlow;
+  }
+
+  /**.
+   * Getter method for fuel flow
+   *
+   * @return fuelFlow the current fuelFlow
+   */
+  
+  public double getFuelFlow() {
+    return this.fuelFlow;
+  }
+  /**.
+   * Getter method for current thrust
+   *
+   * @return currThrust the current thrust
+   */
+  
+  public double getCurrThrust() {
+    return this.currThrust;
+  }
+  /**.
+   * Setter method for current fuel flow
+   *
+   * @param fuelFlow the fuel flow to be set
+   */
+  
+  public void setFuelFlow(double fuelFlow) {
+    this.fuelFlow = fuelFlow;
+  }
+  /**.
+   * Setter method for current thrust
+   *
+   * @param thrust the thrust to be set
+   */
+  
+  public void setCurrThrust(double thrust) {
+    this.currThrust = thrust;
+  }
 }
